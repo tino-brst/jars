7977ba43d6daaf8ee7013ad194b247b58a3e22e2
Agustin Burset <agustin.burset@icloud.com>
2024-10-05 09:21:35 -0300

    tweak styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <main>
-      <h1 className="mb-2 text-xl font-bold">Jars 🫙</h1>
+      <h1 className="mb-4 text-xl font-bold">Jars 🫙</h1>
 
       <form action={createJar} className="mb-4">
         <button className="rounded bg-gray-100 px-2">Add Jar</button>
@@ -15,8 +15,7 @@ export default async function Home() {
       <ul className="list-inside list-disc">
         {jars.map((jar) => (
           <li key={jar.id}>
-            <span className="font-medium">{jar.name}</span> ・{' '}
-            {jar.balance / 100}{' '}
+            <span className="font-medium">{jar.name}</span>・{jar.balance / 100}{' '}
             <span className="text-gray-400">{jar.currency}</span>
           </li>
         ))}
