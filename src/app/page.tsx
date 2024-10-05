8009134368a655ec33c970f0d58dda46250227cb
Agustin Burset <agustin.burset@icloud.com>
2024-10-04 22:03:34 -0300

    list jars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <main>Hello there! 👋</main>
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function Home() {
+  const jars = await prisma.jar.findMany()
+
+  return (
+    <main>
+      <h1>Jars 🫙</h1>
+      <ul>
+        {jars.map((jar) => (
+          <li key={jar.id}>{jar.name}</li>
+        ))}
+      </ul>
+    </main>
+  )
 }
