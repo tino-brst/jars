e83a80b6bb9a6e5be1580da8022e56b36841328f
Agustin Burset <agustin.burset@icloud.com>
2024-10-05 20:11:05 -0300

    tweak jars styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ async function Home() {
 
   return (
     <main>
-      <h1 className="mb-4 text-xl font-bold">Jars 🫙</h1>
+      <h1 className="mb-4 text-xl font-bold">Jars</h1>
 
       <form action={createJar} className="mb-4 flex flex-col gap-2">
         <fieldset className="flex gap-2">
@@ -21,6 +21,7 @@ async function Home() {
             placeholder="name"
             className="flex-1 rounded border"
           />
+
           <select name="currency" defaultValue="USD" required>
             <option value="USD">USD</option>
             <option value="ARS">ARS</option>
@@ -31,17 +32,20 @@ async function Home() {
         <button className="rounded bg-gray-100 px-2">Add Jar</button>
       </form>
 
-      <ul className="space-y-2">
+      <ol className="grid grid-cols-[repeat(auto-fill,minmax(min(8rem,100%),1fr))] gap-4">
         {jars.map((jar) => (
-          <li key={jar.id}>
-            <span className="font-medium">{jar.name}</span>・{jar.balance / 100}{' '}
-            <span className="text-gray-400">{jar.currency}</span>
-            <p className="text-sm text-gray-300">
-              {jar.createdAt.toLocaleString()}
-            </p>
+          <li
+            key={jar.id}
+            className="flex flex-col gap-1 rounded-xl bg-gray-100 px-3 py-2"
+          >
+            <p className="truncate font-medium">{jar.name}</p>
+            <div className="flex items-baseline gap-1">
+              <p className="text-2xl font-medium">{jar.balance / 100}</p>
+              <p className="text-gray-400">{jar.currency}</p>
+            </div>
           </li>
         ))}
-      </ul>
+      </ol>
     </main>
   )
 }
