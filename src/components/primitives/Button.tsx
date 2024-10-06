8365fe335d59c458fb419a367d3f7359dbe5b782
Agustin Burset <agustin.burset@icloud.com>
2024-10-06 09:04:32 -0300

    add disabled styling to button primitive

--- src/components/primitives/Button.tsx
+++ src/components/primitives/Button.tsx
@@ -5,7 +5,8 @@ function Button({ className, ...props }: ComponentPropsWithRef<'button'>) {
   return (
     <button
       className={twMerge(
-        'rounded-full border bg-white px-3 py-0.5 font-medium shadow-sm',
+        'rounded-full border bg-white px-3 py-0.5 font-medium shadow-sm transition-colors',
+        'disabled:bg-gray-50 disabled:text-gray-500',
         className,
       )}
       {...props}
