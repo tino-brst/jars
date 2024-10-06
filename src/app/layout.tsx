5b7d4b30385943231ac4c6e6aafda2ae43ff918e
Agustin Burset <agustin.burset@icloud.com>
2024-10-06 17:05:17 -0300

    add padding b to account for nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { NavLink } from '@/components/NavLink'
+import { twMerge } from 'tailwind-merge'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -17,7 +18,14 @@ const metadata: Metadata = {
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="p-4">
+      <body
+        className={twMerge(
+          // TODO maybe the nav can be sticky? So there is no need to keep track
+          // of its height. The body will need min-h-screen or similar though
+          '[--nav-height:52px]',
+          'p-4 pb-[calc(env(safe-area-inset-bottom)+theme(spacing.4)+var(--nav-height))]',
+        )}
+      >
         {children}
 
         <nav className="fixed inset-x-0 bottom-0 flex bg-white/75 ring-1 ring-black/5 backdrop-blur-xl">
