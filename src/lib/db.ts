30dd0d240d4bc97a6f1438af4894c31b6ea7d575
Agustin Burset <agustin.burset@icloud.com>
2024-10-05 23:15:49 -0300

    fix max connections limit error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { prisma } from './prisma'
 
-const db = new PrismaClient()
+const db = prisma
 
 export { db }
---
+++ src/lib/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+
+// Reference
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export { prisma }
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
