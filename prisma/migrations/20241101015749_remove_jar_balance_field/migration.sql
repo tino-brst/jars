521590bdb3c44027bb05d26a618b880c03e0966e
Agustin Burset <agustin.burset@icloud.com>
2024-10-31 23:07:15 -0300

    remove jar balance field

---
+++ prisma/migrations/20241101015749_remove_jar_balance_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `balance` on the `Jar` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Jar" DROP COLUMN "balance";
