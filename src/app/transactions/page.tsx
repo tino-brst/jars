24a39945cb60a4223ae4b61165ed9c1da96c83d1
Agustin Burset <agustin.burset@icloud.com>
2024-11-01 07:29:13 -0300

    add missing keys

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -11,7 +11,7 @@ import {
   ReceivedTransaction,
   Jar,
 } from '@prisma/client'
-import React from 'react'
+import React, { Fragment } from 'react'
 
 type Transaction = Omit<BaseTransaction, 'type'> & { jar: Jar } & (
     | (Omit<ReceivedTransaction, 'transactionId' | 'jarId'> & {
@@ -116,12 +116,9 @@ async function Transactions() {
 
       <ol className="flex flex-col gap-2">
         {transactions.map((transaction) => (
-          <>
+          <Fragment key={transaction.id}>
             {transaction.type === 'RECEIVED' && (
-              <li
-                key={transaction.id}
-                className="flex items-center justify-between rounded-xl bg-gray-100 px-3 py-2"
-              >
+              <li className="flex items-center justify-between rounded-xl bg-gray-100 px-3 py-2">
                 <div className="flex items-center gap-4">
                   <div className="flex w-fit items-center justify-center rounded-full bg-gray-200 p-1">
                     <ArrowDownIcon size={24} />
@@ -141,7 +138,7 @@ async function Transactions() {
                 </div>
               </li>
             )}
-          </>
+          </Fragment>
         ))}
       </ol>
     </main>
