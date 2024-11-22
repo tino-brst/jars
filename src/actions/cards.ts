c89c70e5126de5777bcbc103c383235ff8b2d15c
Agustin Burset <agustin.burset@icloud.com>
2024-11-22 09:25:45 -0300

    support multi-jar cards creation & display

--- src/actions/cards.ts
+++ src/actions/cards.ts
@@ -7,23 +7,40 @@ import { revalidatePath } from 'next/cache'
 import { CardIssuer, CardType } from '@prisma/client'
 
 const schema = z.object({
-  jarId: z.string().uuid(),
+  name: z.string(),
   issuer: z.nativeEnum(CardIssuer),
   type: z.nativeEnum(CardType),
   lastFourDigits: z
     .string()
     .length(4)
     .regex(RegExp('\\d{4,4}'), 'Must be 4 digits'),
+  jarIds: z.array(z.string().uuid()).nonempty(),
 })
 
 async function createCard(formData: FormData) {
-  const parse = schema.safeParse(Object.fromEntries(formData))
+  const parse = schema.safeParse({
+    ...Object.fromEntries(formData),
+    jarIds: formData.getAll('jarIds'),
+  })
 
   if (!parse.success) {
     throw parse.error.issues
   }
 
-  await db.card.create({ data: parse.data })
+  const { jarIds, ...data } = parse.data
+
+  await db.card.create({
+    data: {
+      ...data,
+      jars: {
+        create: jarIds.map((jarId) => ({
+          jar: {
+            connect: { id: jarId },
+          },
+        })),
+      },
+    },
+  })
 
   revalidatePath('/', 'layout')
 }
--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,16 +1,9 @@
-import { createCard } from '@/actions/cards'
-import { AddCardSubmitButton } from '@/components/AddCardSubmitButton'
-import { Input } from '@/components/primitives/Input'
-import { Select } from '@/components/primitives/Select'
+import { NewCardForm } from '@/components/NewCardForm'
 import { db } from '@/lib/db'
-import { CardIssuer, CardType } from '@prisma/client'
 import React from 'react'
 
 async function Cards() {
   const cards = await db.card.findMany({
-    include: {
-      jar: true,
-    },
     orderBy: {
       createdAt: 'desc',
     },
@@ -26,48 +19,7 @@ async function Cards() {
     <main>
       <h1 className="mb-4 text-3xl font-bold">Cards</h1>
 
-      <form className="mb-6 flex flex-col gap-2" action={createCard}>
-        <div className="flex flex-col gap-2">
-          <div className="flex gap-2">
-            <Select
-              name="issuer"
-              defaultValue={CardIssuer.VISA}
-              required
-              className="flex-1"
-            >
-              <option value={CardIssuer.VISA}>Visa</option>
-              <option value={CardIssuer.MASTERCARD}>Mastercard</option>
-            </Select>
-            <Select
-              name="type"
-              defaultValue={CardType.DEBIT}
-              required
-              className="flex-1"
-            >
-              <option value={CardType.DEBIT}>Debit</option>
-              <option value={CardType.CREDIT}>Credit</option>
-            </Select>
-          </div>
-          <Input
-            type="text"
-            name="lastFourDigits"
-            required
-            minLength={4}
-            maxLength={4}
-            pattern="\d{4,4}"
-            autoComplete="off"
-          />
-          <Select required name="jarId">
-            {jars.map((jar) => (
-              <option value={jar.id} key={jar.id}>
-                {jar.name} ({jar.currency})
-              </option>
-            ))}
-          </Select>
-        </div>
-
-        <AddCardSubmitButton />
-      </form>
+      <NewCardForm jars={jars} />
 
       <ol className="grid grid-cols-[repeat(auto-fill,minmax(min(10rem,100%),1fr))] gap-2">
         {cards.map((card) => (
@@ -75,12 +27,7 @@ async function Cards() {
             key={card.id}
             className="flex aspect-video flex-col justify-between rounded-lg bg-gray-100 px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-200"
           >
-            <div className="flex items-center justify-between">
-              <p className="text-base font-medium">{card.jar.name}</p>
-              <p className="text-sm font-medium text-gray-400">
-                {card.jar.currency}
-              </p>
-            </div>
+            <p className="text-base font-medium">{card.name}</p>
             <div className="flex items-baseline justify-between text-sm">
               <p>
                 •••• <span>{card.lastFourDigits}</span>
