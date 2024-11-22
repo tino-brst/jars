3625a8eb3b4c538d79a5d75324939349dc93c7f7
Agustin Burset <agustin.burset@icloud.com>
2024-11-22 19:08:36 -0300

    add account model

---
+++ prisma/migrations/20241122220704_add_account_model/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
