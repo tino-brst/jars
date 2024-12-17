034efab580f660c8064dde891c1d61d25c9b5edd
Agustin Burset <agustin.burset@icloud.com>
2024-12-17 13:04:27 -0300

    fix select bg color & target size

--- src/components/primitives/Select.tsx
+++ src/components/primitives/Select.tsx
@@ -12,7 +12,7 @@ function Select({
       <select
         className={twMerge(
           'w-full cursor-pointer appearance-none truncate rounded-full border bg-white py-0.5 pl-3 pr-7 font-medium text-black shadow-sm',
-          '[label_&]:-my-0.5 [label_&]:rounded-l-none [label_&]:border-none [label_&]:bg-none [label_&]:pl-0 [label_&]:shadow-none [label_&]:outline-none [label_&]:[text-align-last:right]',
+          '[label_&]:-my-0.5 [label_&]:rounded-l-none [label_&]:border-none [label_&]:bg-transparent [label_&]:pl-2 [label_&]:shadow-none [label_&]:outline-none [label_&]:[text-align-last:right]',
         )}
         {...props}
       >
