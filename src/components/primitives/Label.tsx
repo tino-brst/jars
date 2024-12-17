6d00fa84fbc2de86a92a673c46677ada7bbe78cd
Agustin Burset <agustin.burset@icloud.com>
2024-12-17 11:41:02 -0300

    update select to support label primitive, remove icon pointer events

--- src/components/primitives/Label.tsx
+++ src/components/primitives/Label.tsx
@@ -5,7 +5,7 @@ function Label({ className, ...props }: ComponentPropsWithRef<'label'>) {
   return (
     <label
       className={twMerge(
-        'flex items-center justify-between rounded-full border bg-white py-0.5 pl-3 shadow-sm [&:has(:focus-visible)]:outline-blue-500',
+        'flex items-center justify-between gap-2 whitespace-nowrap rounded-full border bg-white py-0.5 pl-3 font-medium text-gray-500 shadow-sm [&:has(:focus-visible)]:outline-blue-500',
         className,
       )}
       {...props}
--- src/components/primitives/Select.tsx
+++ src/components/primitives/Select.tsx
@@ -2,10 +2,6 @@ import { ComponentPropsWithRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { ChevronSelectorVerticalIcon } from '../icons/ChevronSelectorVerticalIcon'
 
-// TODO ✋ add label (like the iOS one, with the label on the left, and the
-// selected value on the right). When wrapping a select with a label, do up and
-// down arrows still work?
-
 function Select({
   className,
   children,
@@ -14,13 +10,16 @@ function Select({
   return (
     <div className={twMerge('relative flex min-w-0 items-center', className)}>
       <select
-        className="w-full cursor-pointer appearance-none truncate rounded-full border bg-white py-0.5 pl-3 pr-7 font-medium shadow-sm"
+        className={twMerge(
+          'w-full cursor-pointer appearance-none truncate rounded-full border bg-white py-0.5 pl-3 pr-7 font-medium text-black shadow-sm',
+          '[label_&]:-my-0.5 [label_&]:rounded-l-none [label_&]:border-none [label_&]:bg-none [label_&]:pl-0 [label_&]:shadow-none [label_&]:outline-none [label_&]:[text-align-last:right]',
+        )}
         {...props}
       >
         {children}
       </select>
       <ChevronSelectorVerticalIcon
-        className="absolute right-2 text-gray-500"
+        className="pointer-events-none absolute right-2 text-gray-500"
         size={18}
       />
     </div>
